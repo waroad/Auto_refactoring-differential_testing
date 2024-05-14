5779de4ef6f5573b6c7515f6a6aa1202dda6d6f3
Doha Nam <78641034+waroad@users.noreply.github.com>
2024-05-14 16:47:55 +0900

    add examples

--- examples/example1.py
+++ examples/example1.py
@@ -2,17 +2,27 @@ list_1 = []
 list_2 = ["ha", "haha"]
 dict_1={}
 set_1=set()
-temp=14
-for j in range(9):
+temp=2
+for j in range(10):
     dict_1[str(j)]=j
-for k in range(8):
-    set_1.add(k)
-    list_1.append(k*temp)
-    dict_1["kk"]=10
-    print("h")
-for i in range(12):
-    list_2.append(i*temp)
+    set_1.add(j)
+    list_1.append(j*temp)
 
 print(list_1, "sum:",sum(list_1))
 print(dict_1)
 print(set_1, "sum:",sum(set_1))
+
+# 변환 후
+# 
+# list_1 = []
+# list_2 = ['ha', 'haha']
+# dict_1 = {}
+# set_1 = set()
+# temp = 2
+# temp_dict_1 = {str(j): j for j in range(10)}
+# dict_1.update(temp_dict_1)
+# set_1 |= {j for j in range(10)}
+# list_1 += [j * temp for j in range(10)]
+# print(list_1, 'sum:', sum(list_1))
+# print(dict_1)
+# print(set_1, 'sum:', sum(set_1))
--- examples/example3.py
+++ examples/example3.py
@@ -1,34 +1,10 @@
-
-list_1 = []
-list_2 = []
-temp=14
-list_2.append(3)
-for i in range(12):
-    list_2.append(i*temp)
-    list_1.append(i)
-    print("hi")
-    temp+=1
-
-list_2.append(3)
-
-for i in range(4):
-    list_1.append(i)
-
-print(list_1)
+name = "Joe"
+if name == "Casey" or name == "Joe" or name == "Mike":
+    print("yes")
 
 
-
-list1=[]
-for i in range(5):
-    list1.append(i)
-    list_2.append(5)
-for i in range(3):
-    list1.append(i*i)
-
-print(list_1,list1,list_2)
-
-
-
-list3=[]
-for j in range(4):
-    list3.append(j+temp)
+# 변환 후
+#
+# name = "Joe"
+# if name in ["Casey", "Joe", "Mike"]:
+#     print("yes")
