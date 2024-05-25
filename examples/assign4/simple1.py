6cc50310d72760863cade2d3dd378a20cc119334
JeongSeongwon99 <wjdtjddnjs99@kaist.ac.kr>
2024-05-25 15:44:08 +0900

    update dir

---
+++ examples/assign4/simple1.py
@@ -0,0 +1,37 @@
+def bar1():
+    a = 3
+    b = 4
+    c = a + b
+    return -c
+
+def bar2():
+    a = 'foo'
+    b = 'bar'
+    return a + b
+
+def bar3(x, y, c):
+    y = -1 * y
+    z = (x + y) * c
+    return z
+
+def bar4(a, b):
+    if a > b:
+        return a + b
+    elif a == b:
+        return False
+    else:
+        return True
+
+def bar5(a):
+    a += 1
+    return bar3(3, 4, a)
+
+def bar6():
+    a = bar1()
+    return a
+
+def bar7(a, b, c):
+    if a > b:
+        return a | c
+    else:
+        return a & c
