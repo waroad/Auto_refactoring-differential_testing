0560f1c338ba66153b050dd6c035cddb452cce15
JeongSeongwon99 <wjdtjddnjs99@kaist.ac.kr>
2024-05-28 21:59:39 +0900

    testcase update

--- examples/simple/diff_example1.py
+++ examples/simple/diff_example1.py
@@ -1,7 +1,11 @@
+import time
+start_time = time.time()
+
 list_1 = []
+list_2 = ["ha", "haha"]
 dict_1={}
 set_1=set()
-for i in range(10):
+for i in range(100000):
     list_1.append(i)
     dict_1[str(i)]=i
     set_1.add(i)
@@ -9,3 +13,7 @@ for i in range(10):
 print(list_1, "sum:",sum(list_1))
 print(dict_1)
 print(set_1, "sum:",sum(set_1))
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+print(f"Total execution time: {elapsed_time} seconds")
---
+++ examples/simple/diff_example4.py
@@ -0,0 +1,11 @@
+# merge append
+A = []
+A.append(1)
+A.append(2)
+A.append(3)
+    
+# changed 1
+A=[1, 2, 3]
+# changed 2
+A = []
+A += [1, 2, 3]
