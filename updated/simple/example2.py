ff312ae15e11710c197a376db02ffd7eda75c320
차민호 <71549133+Cha-Minho@users.noreply.github.com>
2024-05-30 21:46:06 +0900

    trash out

--- updated/simple/example2.py
+++
@@ -1,9 +0,0 @@
-(a, b, c, d, e) = (2, 3, 5, 7, 1)
-if a <= b <= c:
-    print('changed')
-elif c > b > a:
-    print('changed')
-elif a > b < c:
-    print('not changed')
-else:
-    print('changed' if (e > c and a > b >= c >= d or d > e > 10) and c > 1 > 0 else 'ignore')
