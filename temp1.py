447d290b81f6f61501c3b6c5ef3c918be341fca4
Doha Nam <78641034+waroad@users.noreply.github.com>
2024-06-12 23:52:00 +0900

    rename bench

--- temp1.py
+++ temp1.py
@@ -1,41 +1,16 @@
-# BOJ 1296. 팀 이름 정하기
+# https://www.acmicpc.net/source/79493123
+
 import sys
 
-
-# 특정 글자의 수를 세주는 함수
-def find_letter(word, letter):
-    count = 0
-
-    for i in range(len(word)):
-        if word[i] == letter:
-            count += 1
-
-    return count
-
-
-# 입력과, 연두 이름의 L, O, V, E 값을 세기
-yeondu = input()
-teams, winning = [], {}
+input = sys.stdin.readline
+stk, cnt, = [], 1
 N = int(input())
-
 for _ in range(N):
-    teams.append(sys.stdin.readline().rstrip())
+    stk.append(int(input()))
+last = stk[-1]
 
-teams.sort()
-
-# 팀 이름을 바탕으로 확률을 계산하여 사전에 저장
-for _ in range(N):
-    team = teams[_]
-    L = find_letter(yeondu, 'L') + find_letter(team, 'L')
-    O = find_letter(yeondu, 'O') + find_letter(team, 'O')
-    V = find_letter(yeondu, 'V') + find_letter(team, 'V')
-    E = find_letter(yeondu, 'E') + find_letter(team, 'E')
-    score = (L + O) * (O + V) * (V + E) * (L + V) * (O + E) * (L + E)
-    winning[team] = score % 100
-
-max_score = max(winning.values())
-
-for _ in range(N):
-    if winning[teams[_]] == max_score:
-        print(teams[_])
-        break
+for i in reversed(range(N)):
+    if stk[i] > last :
+        cnt +=1
+        last = stk[i]
+print(cnt)
--- temp2.py
+++ temp2.py
@@ -1,21 +1,10 @@
 import sys
-
-def find_letter(word, letter):
-    count = 0
-    for (i, item) in enumerate(word):
-        if item == letter:
-            count += 1
-    return count
-(yeondu, (teams, winning), N) = (input(), ([], {}), int(input()))
-teams += [sys.stdin.readline().rstrip() for _ in range(N)]
-teams.sort()
-for _ in range(N):
-    team = teams[_]
-    (L, O, V, E) = (find_letter(yeondu, 'L') + find_letter(team, 'L'), find_letter(yeondu, 'O') + find_letter(team, 'O'), find_letter(yeondu, 'V') + find_letter(team, 'V'), find_letter(yeondu, 'E') + find_letter(team, 'E'))
-    score = (L + O) * (O + V) * (V + E) * (L + V) * (O + E) * (L + E)
-    winning[team] = score % 100
-max_score = max(winning.values())
-for _ in range(N):
-    if winning[teams[_]] == max_score:
-        print(teams[_])
-        break
+(input, (stk, cnt)) = (sys.stdin.readline, ([], 1))
+N = int(input())
+stk += [int(input()) for _ in range(N)]
+last = stk[-1]
+for i in reversed(range(N)):
+    if stk[i] > last:
+        cnt += 1
+        last = stk[i]
+print(cnt)
